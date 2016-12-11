2bf5cdedcd900065ca1cdfc296c0f3a6e876068b
Agop Shirinian <agop@triller.co>
2016-12-10 19:49:32 -0500

    Initial commit of build files after running `edk-build --platform=iphone`

---
+++ source/generic/s3eNativeShare.cpp
@@ -0,0 +1,29 @@
+/*
+Generic implementation of the s3eNativeShare extension.
+This file should perform any platform-indepedentent functionality
+(e.g. error checking) before calling platform-dependent implementations.
+*/
+
+/*
+ * NOTE: This file was originally written by the extension builder, but will not
+ * be overwritten (unless --force is specified) and is intended to be modified.
+ */
+
+
+#include "s3eNativeShare_internal.h"
+s3eResult s3eNativeShareInit()
+{
+    //Add any generic initialisation code here
+    return s3eNativeShareInit_platform();
+}
+
+void s3eNativeShareTerminate()
+{
+    //Add any generic termination code here
+    s3eNativeShareTerminate_platform();
+}
+
+s3eResult s3eNativeShareShow(const char* text, const char* url)
+{
+	return s3eNativeShareShow_platform(text, url);
+}
---
+++ source/iphone/s3eNativeShare_platform.mm
@@ -0,0 +1,24 @@
+/*
+ * iphone-specific implementation of the s3eNativeShare extension.
+ * Add any platform-specific functionality here.
+ */
+/*
+ * NOTE: This file was originally written by the extension builder, but will not
+ * be overwritten (unless --force is specified) and is intended to be modified.
+ */
+#include "s3eNativeShare_internal.h"
+
+s3eResult s3eNativeShareInit_platform()
+{
+    // Add any platform-specific initialisation code here
+    return S3E_RESULT_SUCCESS;
+}
+
+void s3eNativeShareTerminate_platform()
+{ 
+}
+
+s3eResult s3eNativeShareShow_platform(const char* text, const char* url)
+{
+    return S3E_RESULT_ERROR;
+}
